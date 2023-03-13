5620d325c66f28edfad320db02ef62f4af291d17
Dylan <dylan.imail@gmail.com>
2023-03-13 11:28:54 -0400

    readme update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import { signIn, signOut, useSession } from "next-auth/react";
 export default function Home() {
   const { data: session, status } = useSession();
   if (status === "loading") {
-    return <main>loading...</main>;
+    return <main className="flex flex-col items-center">loading...</main>;
   }
   return (
     <main className="flex flex-col items-center">
-      <h1>Yolki</h1>
+      <h1 className="pt-8 text-3xl">Yolki</h1>
       <Login session={session} />
     </main>
   );
