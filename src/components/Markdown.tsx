363e2ec596e40096a30018ce98de962256b102f6
Dylan <dylan.imail@gmail.com>
2023-03-28 16:21:11 -0400

    created back card component and document split function

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -3,16 +3,24 @@ import MarkdownEditor from "./MarkdownEditor";
 import MarkdownView from "./MarkdownView";
 
 export default function Markdown() {
-    const [doc, setDoc] = useState('```js\nconsole.log("Hello World");\n```' + "\n".repeat(10));
+    const [docFront, setDocFront] = useState<string>('```js\nconsole.log("Hello World");\n```' + "\n".repeat(10));
+    const [docBack, setDocBack] = useState<string>('');
 
     const handleDocChange = useCallback((newDoc: string) => {
-        setDoc(newDoc);
+        const splitDoc = newDoc.split("---back---");
+        setDocFront(splitDoc[0] ? splitDoc[0] : "");
+        setDocBack(splitDoc[1] ? splitDoc[1] : "");
     }, []);
 
     return (
-        <div className="flex mt-4 px-2 gap-2">
-            <MarkdownEditor initialDoc={doc} onChange={handleDocChange} />
-            <MarkdownView doc={doc} />
-        </div>
+        <>
+            <div className="mt-4 flex gap-2 px-2">
+                <MarkdownEditor initialDoc={docFront} onChange={handleDocChange} />
+                <MarkdownView doc={docFront} />
+            </div>
+            <div className="mt-4 flex gap-2 px-2">
+                <MarkdownView doc={docBack} />
+            </div>
+        </>
     );
 }
