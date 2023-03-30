8d5087b7d41918cfd6a83a243d1c9edb74b0bf4d
Dylan <dylan.imail@gmail.com>
2023-03-30 11:33:11 -0400

    styled decklist

--- src/components/DeckList.tsx
+++ src/components/DeckList.tsx
@@ -16,24 +16,32 @@ export default function DeckList() {
     return (
         <div className="mt-3 flex flex-col gap-4">
             <h2>Decks</h2>
-            {deckList?.map((deck, idx) => {
-                return (
-                    <div key={idx} className="flex justify-between">
-                        <u className="mr-2">{deck.name}</u>
-                        <button
-                            className="rounded-md border-2 border-zinc-800 p-1 focus:outline-none"
-                            onClick={() => {
-                                deleteDeck.mutate({
-                                    id: deck.id,
-                                    name: deck.name,
-                                });
-                            }}
-                        >
-                            Delete
-                        </button>
-                    </div>
-                );
-            })}
+            <ul>
+                {deckList?.map((deck, idx) => {
+                    return (
+                        <li key={idx} className="mb-2">
+                            <p className="mr-2">{deck.name}</p>
+                            <ul className="ml-4 pl-2 border-l border-neutral-800">
+                                <li className="mt-1 hover:cursor-pointer rounded-md px-2 pt-0.5 pb-1 hover:bg-neutral-800">Create</li>
+                                <li className="mt-1 hover:cursor-pointer rounded-md px-2 pt-0.5 pb-1 hover:bg-neutral-800">Manage</li>
+                                <li className="mt-1 hover:cursor-pointer rounded-md px-2 pt-0.5 pb-1 hover:bg-neutral-800">
+                                    <button
+                                        className="hover:cursor-pointer"
+                                        onClick={() => {
+                                            deleteDeck.mutate({
+                                                id: deck.id,
+                                                name: deck.name,
+                                            });
+                                        }}
+                                    >
+                                        Delete
+                                    </button>
+                                </li>
+                            </ul>
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 }
