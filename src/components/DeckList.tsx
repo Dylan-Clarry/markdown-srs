8b1ab0836116aaa8476b9ca1865438bce17186de
Dylan <dylan.imail@gmail.com>
2023-05-26 22:14:31 -0400

    collapsible list (kind of)

--- src/components/DeckList.tsx
+++ src/components/DeckList.tsx
@@ -1,13 +1,13 @@
 import { api } from "../utils/api";
+import { useState } from "react";
+
+interface IDeck {
+    id: string,
+    name: string
+}
 
 export default function DeckList() {
     const { data: deckList, isLoading } = api.deck.getAllDecks.useQuery();
-    const utils = api.useContext();
-    const deleteDeck = api.deck.deleteDeck.useMutation({
-        onSettled: async () => {
-            await utils.deck.invalidate();
-        },
-    });
 
     if (isLoading) {
         return <div className="mt-3 flex flex-col gap-4 text-center">Fetching decks...</div>;
@@ -16,32 +16,53 @@ export default function DeckList() {
     return (
         <div className="mt-3 flex flex-col gap-4">
             <ul>
-                {deckList?.map((deck, idx) => {
+                {deckList?.map((deck: IDeck, idx: number) => {
                     return (
-                        <li key={idx} className="mb-2">
-                            <p className="mr-2">{deck.name}</p>
-                            <ul className="ml-4 pl-2 border-l border-neutral-800">
-                                <li className="mt-1 hover:cursor-pointer rounded-md px-2 pt-0.5 pb-1 hover:bg-neutral-800">Review</li>
-                                <li className="mt-1 hover:cursor-pointer rounded-md px-2 pt-0.5 pb-1 hover:bg-neutral-800">Create</li>
-                                <li className="mt-1 hover:cursor-pointer rounded-md px-2 pt-0.5 pb-1 hover:bg-neutral-800">Manage</li>
-                                <li className="mt-1 hover:cursor-pointer rounded-md px-2 pt-0.5 pb-1 hover:bg-neutral-800">
-                                    <button
-                                        className="hover:cursor-pointer"
-                                        onClick={() => {
-                                            deleteDeck.mutate({
-                                                id: deck.id,
-                                                name: deck.name,
-                                            });
-                                        }}
-                                    >
-                                        Delete
-                                    </button>
-                                </li>
-                            </ul>
-                        </li>
+                        <CollapsableList deck={deck} idx={idx}/>
                     );
                 })}
             </ul>
         </div>
+    );
+}
+
+function CollapsableList(props: {deck: any, idx: number}) {
+    const { deck, idx } = props;
+    const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
+    const handleCollapse = () => {
+        setIsCollapsed((isCollapsed) => !isCollapsed);
+    };
+    const utils = api.useContext();
+    const deleteDeck = api.deck.deleteDeck.useMutation({
+        onSettled: async () => {
+            await utils.deck.invalidate();
+        },
+    });
+
+    return (
+        <li key={idx} className="mb-2">
+            <p className="mr-2" onClick={handleCollapse}> {deck.name}</p>
+            <ul
+                className="ml-4 border-l border-neutral-800 pl-2"
+                style={{ visibility: isCollapsed ? "visible" : "collapse" }}
+            >
+                <li className="mt-1 rounded-md px-2 pt-0.5 pb-1 hover:cursor-pointer hover:bg-neutral-800">Review</li>
+                <li className="mt-1 rounded-md px-2 pt-0.5 pb-1 hover:cursor-pointer hover:bg-neutral-800">Create</li>
+                <li className="mt-1 rounded-md px-2 pt-0.5 pb-1 hover:cursor-pointer hover:bg-neutral-800">Manage</li>
+                <li className="mt-1 rounded-md px-2 pt-0.5 pb-1 hover:cursor-pointer hover:bg-neutral-800">
+                    <button
+                        className="hover:cursor-pointer"
+                        onClick={() => {
+                            deleteDeck.mutate({
+                                id: deck.id,
+                                name: deck.name,
+                            });
+                        }}
+                    >
+                        Delete
+                    </button>
+                </li>
+            </ul>
+        </li>
     );
 }
