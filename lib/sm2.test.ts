cfcb15229bf70cdd55f625b9198b50ec1555ea27
Dylan <dylan.imail@gmail.com>
2023-08-23 11:38:31 -0400

    Card scheduling testing complete

--- lib/sm2.test.ts
+++ lib/sm2.test.ts
@@ -1,30 +1,47 @@
 export {};
 import { sm2, cardData } from "./sm2";
 
-describe("sm2 algorithm test", () => {
-    it("Grade card 5", () => {
-        const cardData: cardData = {
-            repetition: 2,
-            interval: 6,
-            eFactor: 1.3,
-        };
-        expect(sm2(5, cardData)).toEqual({
-            repetition: 3,
-            interval: 8,
-            eFactor: 1.4000000000000001,
-        });
-    });
+type Algorithm = (grade: number, data: cardData) => cardData;
 
-    it("Grade card 0", () => {
-        const cardData: cardData = {
+function testAlgorithm(algorithmName: string, algorithm: Algorithm) {
+    describe(algorithmName + " algorithm test", () => {
+        // let statement is used to test grading the same card multiple times
+        let cardData: cardData = {
             repetition: 0,
             interval: 0,
             eFactor: 2.5,
         };
-        expect(sm2(0, cardData)).toEqual({
-            repetition: 0,
-            interval: 1,
-            eFactor: 1.3,
+
+        it("Grade card 5", () => {
+            cardData = algorithm(5, cardData);
+            expect(cardData).toEqual({ repetition: 1, interval: 1, eFactor: 2.6 });
+        });
+
+        it("Grade card 4", () => {
+            cardData = algorithm(4, cardData);
+            expect(cardData).toEqual({ repetition: 2, interval: 6, eFactor: 2.6 });
+        });
+
+        it("Grade card 3", () => {
+            cardData = algorithm(3, cardData);
+            expect(cardData).toEqual({ repetition: 3, interval: 16, eFactor: 2.46 });
+        });
+
+        it("Grade card 2", () => {
+            cardData = algorithm(2, cardData);
+            expect(cardData).toEqual({ repetition: 0, interval: 1, eFactor: 2.1399999999999997 });
+        });
+
+        it("Grade card 1", () => {
+            cardData = algorithm(1, cardData);
+            expect(cardData).toEqual({ repetition: 0, interval: 1, eFactor: 1.5999999999999996 });
+        });
+
+        it("Grade card 0", () => {
+            cardData = algorithm(0, cardData);
+            expect(cardData).toEqual({ repetition: 0, interval: 1, eFactor: 1.3 });
         });
     });
-});
+}
+
+testAlgorithm("sm2", sm2);
--- lib/sm2.ts
+++ lib/sm2.ts
@@ -1,7 +1,7 @@
 export type cardData = {
-    repetition: number,
-    eFactor: number,
-    interval: number,
+    repetition: number;
+    eFactor: number;
+    interval: number;
 };
 
 export function sm2(grade: number, { repetition, eFactor, interval }: cardData): cardData {
@@ -9,21 +9,21 @@ export function sm2(grade: number, { repetition, eFactor, interval }: cardData):
     let newInterval: number;
     let newEFactor: number;
 
-    if(grade >= 3) {
-        if(repetition === 0) {
+    if (grade >= 3) {
+        if (repetition === 0) {
             newInterval = 1;
-        } else if(repetition === 1) {
+        } else if (repetition === 1) {
             newInterval = 6;
         } else {
             newInterval = Math.round(interval * eFactor);
         }
-        newRepetition = repetition++;
+        newRepetition = ++repetition;
     } else {
         newRepetition = 0;
         newInterval = 1;
     }
     newEFactor = eFactor + (0.1 - (5 - grade) * (0.08 + (5 - grade) * 0.02));
-    if(newEFactor < 1.3) {
+    if (newEFactor < 1.3) {
         newEFactor = 1.3;
     }
 
