70f1b2eb480c0208118df7723af564fd9b23430e
Dylan <dylan.imail@gmail.com>
2023-08-30 09:04:03 -0400

    Updated datelib tests

--- lib/datelib.test.ts
+++ lib/datelib.test.ts
@@ -1,17 +1,19 @@
 export {};
-import { calculateNewReviewDate, dateDifferenceInDays, addOneDay, subtractOneDay } from "./datelib";
+import { addDaysToReviewDate, dateDifferenceInDays, addOneDay, subtractOneDay } from "./datelib";
 
 describe("Date calculations test", () => {
     const testDate = new Date(2023, 5, 22);
     const testDate2 = new Date(2023, 7, 15);
+    const testDate3 = new Date(2023, 11, 31);
+    const testDate4 = new Date(2023, 11, 28);
 
     it("Add 5 days", () => {
-        const newDate = calculateNewReviewDate(testDate, 5);
+        const newDate = addDaysToReviewDate(testDate, 5);
         expect(newDate).toEqual(new Date(2023, 5, 27));
     });
 
     it("Add days leading into new month", () => {
-        const newDate = calculateNewReviewDate(testDate, 9);
+        const newDate = addDaysToReviewDate(testDate, 9);
         expect(newDate).toEqual(new Date(2023, 6, 1));
     });
 
@@ -28,5 +30,15 @@ describe("Date calculations test", () => {
     it("Subtract one day to date", () => {
         const newDate = subtractOneDay(testDate);
         expect(newDate).toEqual(new Date(2023, 5, 21));
+    });
+
+    it("Adding single day goes into new year", () => {
+        const newDate = addDaysToReviewDate(testDate3, 1);
+        expect(newDate).toEqual(new Date(2024, 0, 1));
+    });
+
+    it("Adding multible days goes into new year", () => {
+        const newDate = addDaysToReviewDate(testDate4, 9);
+        expect(newDate).toEqual(new Date(2024, 0, 6));
     });
 });
--- lib/datelib.ts
+++ lib/datelib.ts
@@ -1,19 +1,31 @@
+import { RouterOutputs } from "../src/utils/api";
+type Card = RouterOutputs["card"]["getSchema"];
+
+export function addDaysToCardReviewDate(card: Card, daysToAdd: number): Card {
+    if(!card) {
+        throw new Error("Error adding days to card review date: card is null");
+    }
+    const newReviewDate = addDaysToReviewDate(card.reviewDate, daysToAdd);
+    card.reviewDate = newReviewDate;
+    return card;
+}
+
 export function dateDifferenceInDays(date1: Date, date2: Date): number {
     const timeDiff = Math.abs(date1.getTime() - date2.getTime());
     const daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
     return daysDiff;
 }
 
-export function calculateNewReviewDate(reviewDate: Date, daysToAdd: number): Date {
+export function addDaysToReviewDate(reviewDate: Date, daysToAdd: number): Date {
     const newReviewDate = new Date(reviewDate.getTime());
     newReviewDate.setDate(newReviewDate.getDate() + daysToAdd);
     return newReviewDate;
 }
 
 export function addOneDay(reviewDate: Date): Date {
-    return calculateNewReviewDate(reviewDate, 1);
+    return addDaysToReviewDate(reviewDate, 1);
 }
 
 export function subtractOneDay(reviewDate: Date): Date {
-    return calculateNewReviewDate(reviewDate, -1);
+    return addDaysToReviewDate(reviewDate, -1);
 }
